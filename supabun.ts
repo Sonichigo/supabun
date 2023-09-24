8ed5ca3b8a8e2c3c3063efe91af06d2506cb62c8
shivamsouravjha <2019145@iiitdmj.ac.in>
2023-09-24 16:22:50 +0000

    feat: creating endpoint for bun

--- supabun.ts
+++ supabun.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia'
+
+const logo  = `
+       ▓██▓▄
+    ▓▓▓▓██▓█▓▄
+     ████████▓▒
+          ▀▓▓███▄      ▄▄   ▄               ▌
+         ▄▌▌▓▓████▄    ██ ▓█▀  ▄▌▀▄  ▓▓▌▄   ▓█  ▄▌▓▓▌▄ ▌▌   ▓
+       ▓█████████▌▓▓   ██▓█▄  ▓█▄▓▓ ▐█▌  ██ ▓█  █▌  ██  █▌ █▓
+      ▓▓▓▓▀▀▀▀▓▓▓▓▓▓▌  ██  █▓  ▓▌▄▄ ▐█▓▄▓█▀ █▓█ ▀█▄▄█▀   █▓█
+       ▓▌                           ▐█▌                   █▌
+        ▓
+`
+const app = new Elysia()
+
+app.get("/hello",()=>{
+    console.log("it came here")
+    return "Hello from supabase"
+})
+
+app.listen("6969")
+
+console.log(`${logo} Elysia is running at on port ${app.server?.port}...`)
