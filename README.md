ddec9721dfbe648e7e8a7d692dcfaf626aa1f455
Animesh <kurosakiichigo.songoku@gmail.com>
2023-09-26 11:22:29 +0530

    feat: bug report with app or keploy
    Signed-off-by: Animesh <kurosakiichigo.songoku@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# supabun
+# Supabun
 
 To install dependencies:
 
@@ -9,7 +9,7 @@ bun install
 To run:
 
 ```bash
-bun run supabun.ts
+bun start
 ```
 
 This project was created using `bun init` in bun v1.0.3. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
--- supabun.ts
+++ supabun.ts
@@ -3,49 +3,43 @@ import HelloController from './controller/hello'
 import * as mongoose from 'mongoose';
 import MongoIntegration from './controller/fetch'
 
-const logo  = `
-       ▓██▓▄
-    ▓▓▓▓██▓█▓▄
-     ████████▓▒
-          ▀▓▓███▄      ▄▄   ▄               ▌
-         ▄▌▌▓▓████▄    ██ ▓█▀  ▄▌▀▄  ▓▓▌▄   ▓█  ▄▌▓▓▌▄ ▌▌   ▓
-       ▓█████████▌▓▓   ██▓█▄  ▓█▄▓▓ ▐█▌  ██ ▓█  █▌  ██  █▌ █▓
-      ▓▓▓▓▀▀▀▀▓▓▓▓▓▓▌  ██  █▓  ▓▌▄▄ ▐█▓▄▓█▀ █▓█ ▀█▄▄█▀   █▓█
-       ▓▌                           ▐█▌                   █▌
-        ▓
-`
 const app = new Elysia()
 
 app.get('/hello', () => {
     const controller = new HelloController();
+    console.log("GET request at /hello route");
     return (controller.sayHello());
 });
 
 app.get('/fetch', () => {
     const controller = new MongoIntegration();
+    console.log("GET request at /fetch route");
     return (controller.Fetch());
 });
 
 app.post('/save', () => {
     const controller = new MongoIntegration();
+    console.log("POST request at /save route");
     return (controller.Save());
 });
 
 app.put('/put', () => {
     const controller = new MongoIntegration();
+    console.log("PUT request at /put route");
     return (controller.Put());
 });
 
 app.delete('/delete', () => {
     const controller = new MongoIntegration();
+    console.log("DELETE request at /delete route");
     return (controller.Delete());
 });
 
-mongoose.connect('mongodb://localhost:27017/keploy', { 
+mongoose.connect('mongodb://mongoDb-bun:27017/keploy', { 
     serverSelectionTimeoutMS: 5000  // timeout after 5 seconds
 })
 
 app.listen("420")
 
 
-console.log(`${logo} Elysia is running at on port ${app.server?.port}...`)
+console.log(`Elysia is running at on port ${app.server?.port}...`)
